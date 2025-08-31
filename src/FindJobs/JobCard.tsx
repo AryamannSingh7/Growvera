7910051c0e8bbbc3bbbdfb9464576fee57596632
Aryamann Singh <aryamann.singh21@gmail.com>
2025-08-31 23:10:13 +0530

    job card complete

--- src/FindJobs/JobCard.tsx
+++ src/FindJobs/JobCard.tsx
@@ -1,5 +1,43 @@
-const JobCard = () => {
-  return <div></div>;
+import { IconBookmark, IconClock } from "@tabler/icons-react";
+import { Divider, Text } from "@mantine/core";
+
+const JobCard = (props: any) => {
+  return (
+    <div className="bg-white-900 p-4 w-80 flex flex-col gap-2 rounded-xl hover:shadow-[0_0_5px_1px_yellow] !shadow-primary-400 transition duration-300 ease-in-out hover:cursor-pointer">
+      <div className="flex gap-2 items-center justify-between">
+        <div className="flex items-center gap-4">
+          <div className="p-2 bg-white-800 rounded-md">
+            <img src={`/Icons/${props.company}.png`} alt="" className="h-7" />
+          </div>
+          <div>
+            <div className="font-semibold">{props.jobTitle}</div>
+            <div className="text-xs text-white-300">
+              {props.company} &#9679; {props.applicants} Applicants
+            </div>
+          </div>
+        </div>
+        <IconBookmark className="text-white-400 cursor-pointer" />
+      </div>
+      <div className="flex gap-2 [&>div]:py-1 [&>div]:px-2 [&>div]:bg-white-800 [&>div]:text-primary-400 [&>div]:rounded-lg text-xs">
+        <div>{props.experience}</div>
+        <div>{props.jobType}</div>
+        <div>{props.location}</div>
+      </div>
+      <Text lineClamp={4} size="xs" className="!text-white-300 ">
+        {props.description}
+      </Text>
+      <Divider size="xs" color="white.7" />
+      <div className="flex justify-between">
+        <div className=" font-semibold text-primary-400">
+          &#8377; {props.package}
+        </div>
+        <div className="flex items-center gap-1 text-white-500 text-xs">
+          <IconClock stroke={1.5} className="h-5 w-5" /> {props.postedDaysAgo}{" "}
+          days ago
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default JobCard;
